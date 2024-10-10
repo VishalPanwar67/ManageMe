86d8126f84afab951bde1c7c09881169fe984837
Vishal Panwar <vishal4panwar@gmail.com>
2024-10-10 20:37:09 +0530

    update path

--- backend/utils/indexUtils.js
+++ backend/utils/indexUtils.js
@@ -1,5 +1,5 @@
 import { asyncHandler } from "./asyncHandler.js";
-import { apiResponse } from "./apiResponse.js";
+import apiResponse from "./apiResponse.js";
 import { apiError } from "./apiError.js";
 import { generateTokenAndSetCookie } from "./generateTokenAndSetCookie.js";
 import { withSocket } from "./withSocket.js";
